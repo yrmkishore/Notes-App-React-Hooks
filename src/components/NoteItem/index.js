9ac45c2feb0548f0fc377049bedcac07c0efdd2e
yrmkishore <yramkishore007@gmail.com>
2023-12-22 13:57:31 +0000

    notes app

---
+++ src/components/NoteItem/index.js
@@ -0,0 +1,14 @@
+import {ListItem, Title, Note} from './styledComponents'
+
+const NoteItem = props => {
+  const {noteDetails} = props
+  const {title, noteText} = noteDetails
+
+  return (
+    <ListItem>
+      <Title>{title}</Title>
+      <Note>{noteText}</Note>
+    </ListItem>
+  )
+}
+export default NoteItem
